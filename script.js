e8dca74e0176462d7e61e6c8971e7eb99a105726
beki1238 <bahibeki@gmail.com>
2024-12-12 17:48:44 +0300

    Java script

---
+++ script.js
@@ -0,0 +1,58 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll',()=>{
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+menu.onclick =()=>{
+ menu.classList.toggle('bx-x');
+ navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+var swiper = new Swiper(".home ",{
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnIntaraction:false,
+    },
+
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+   
+});
+
+var swiper = new Swiper(".coming-container",{
+    spaceBetween: 20,
+    loop: true,
+    // centeredSlides: true,
+    autoplay: {
+        delay: 55000,
+        disableOnIntaraction:false,
+    },
+    centeredSlides:true,
+    breakpoints:{
+        0:{
+            slidesPerview:2,
+        },
+        568:{
+            slidesPerview:3,
+        },
+        768:{
+            slidesPerview:4,
+        },
+        968:{
+            slidesPerview:5,
+        },
+    },
+   
+});
